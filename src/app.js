a96e5a2d7799311803f7fa35c8c61cb232361c42
Safa <sayyujahan@gmail.com>
2026-04-19 13:20:46 +0530

    Updates

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 import express from "express";
 import cookieParser from "cookie-parser";
-import cors from "cors";
 
 import authRoutes from "./routes/auth.routes.js";
 import boardRoutes from "./routes/board.routes.js";
@@ -9,10 +8,7 @@ import taskRoutes from "./routes/task.routes.js";
 
 const app = express();
 
-app.use(cors({
-  origin: process.env.CLIENT_URL || "http://localhost:5173",
-  credentials: true
-}));
+
 
 app.use(express.json());
 app.use(cookieParser());
